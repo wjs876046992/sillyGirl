fd21bdc3b6889fd17bea121806a0fe657cf2f874
cdle <cdle1994@icloud.com>
2023-06-14 17:13:27 +0800

    x

--- core/bucket.go
+++ core/bucket.go
@@ -81,7 +81,7 @@ func MakeBucket(name string) storage.Bucket {
 		})
 		storage.Watch(app, "redis_addr", func(old, new, _ string) *storage.Final {
 			message := "Redis连接成功，重启生效！"
-			err := redis.Try(app.GetString("redis_addr"), app.GetString("redis_password"))
+			err := redis.Try(new, app.GetString("redis_password"))
 			if err != nil {
 				message = "Redis连接失败：" + err.Error()
 			}
@@ -91,7 +91,7 @@ func MakeBucket(name string) storage.Bucket {
 		})
 		storage.Watch(app, "redis_password", func(old, new, _ string) *storage.Final {
 			message := "Redis连接成功，重启生效！"
-			err := redis.Try(app.GetString("redis_addr"), app.GetString("redis_password"))
+			err := redis.Try(app.GetString("redis_addr"), new)
 			if err != nil {
 				message = "Redis连接失败：" + err.Error()
 			}

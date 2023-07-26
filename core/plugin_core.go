4c47c7c3869b7eb872bccc4e2a52318d31782916
1-6 <a1-6@1-6deMacBook-Air.local>
2023-07-26 15:27:10 +0800

    x

--- core/plugin_core.go
+++ core/plugin_core.go
@@ -113,16 +113,18 @@ func initPlugins() {
 		defer pluginLock.Unlock()
 		// fmt.Println("new", new, key)
 
-		if vv, ok := plugins_id.Load(key); ok {
-			filename := vv.(string)
-			if new == "" {
-				os.RemoveAll(filepath.Dir(filename))
-			} else if new != "install" {
-				fmt.Println("WriteFile", []byte(new))
-				os.WriteFile(filename, []byte(new), 0755)
-			}
-			return &storage.Final{
-				Now: "",
+		if new != "install" {
+			if vv, ok := plugins_id.Load(key); ok {
+				filename := vv.(string)
+				if new == "" {
+					os.RemoveAll(filepath.Dir(filename))
+				} else {
+					fmt.Println("WriteFile", []byte(new))
+					os.WriteFile(filename, []byte(new), 0755)
+				}
+				return &storage.Final{
+					Now: "",
+				}
 			}
 		}
 
